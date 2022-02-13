22bc2ab29c35c5604edd136eb9a156caa0b190e3
Mikołaj Leszczuk <leszczuk@agh.edu.pl>
2022-02-13 03:37:32 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,9 +5,9 @@
 # which python
 python setup.py build_apps
 cd build || exit
-rm '/Users/miklesz/Dysk Google/Demo/build/macosx_10_9_x86_64.zip'
-rm '/Users/miklesz/Dysk Google/Demo/build/manylinux2010_x86_64.zip'
-rm '/Users/miklesz/Dysk Google/Demo/build/win_amd64.zip'
-zip -q -r '/Users/miklesz/Dysk Google/Demo/build/macosx_10_9_x86_64.zip' macosx_10_9_x86_64
-zip -q -r '/Users/miklesz/Dysk Google/Demo/build/manylinux2010_x86_64.zip' manylinux2010_x86_64
-zip -q -r '/Users/miklesz/Dysk Google/Demo/build/win_amd64.zip' win_amd64
+rm /Users/miklesz/Dysk\ Google/Demo/build/*.zip
+DATE=`date`
+DATE="${DATE// /_}"
+zip -q -r '/Users/miklesz/Dysk Google/Demo/build/macosx_10_9_x86_64_'$DATE'.zip' macosx_10_9_x86_64
+zip -q -r '/Users/miklesz/Dysk Google/Demo/build/manylinux2010_x86_64_'$DATE'.zip' manylinux2010_x86_64
+zip -q -r '/Users/miklesz/Dysk Google/Demo/build/win_amd64_'$DATE'.zip' win_amd64
